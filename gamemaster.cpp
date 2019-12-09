3ece2c0fd0364acd1c15f7a158613c9b74d55082
chrislample <clample@crimson.ua.edu>
2019-12-09 09:20:11 +0000

    Add code for evaluating guess

--- gamemaster.cpp
+++ gamemaster.cpp
@@ -3,24 +3,43 @@
 #include <string>
 #include "gamemaster.h"
 
-GameMaster* GameMaster::random_solution(int num_spaces, int num_colors) {
+GameMaster* GameMaster::random_solution(int num_spaces, unsigned char num_colors) {
   srand(time(0));
-  char solution[num_spaces];
-  for (int pos = 0; pos < num_spaces; pos++) {
-    solution[pos] = rand() % num_colors;
+  unsigned char solution[num_spaces];
+  int color_count[num_colors] = {0};
+  for (int i = 0; i < num_spaces; i++) {
+    unsigned char color = rand() % num_colors;
+    solution[i] = rand() % num_colors;
+    color_count[color]++;
   }
-  return new GameMaster(solution, num_spaces, num_colors);
+  return new GameMaster(solution, num_spaces, num_colors, color_count);
 }
 
-GameMaster::GameMaster(char* sol, int ns, int nc)
-  :solution(sol), num_spaces(ns), num_colors(nc) {}
+GameMaster::GameMaster(unsigned char* sol, int ns, unsigned char nc, int* col_count)
+  :solution(sol), num_spaces(ns), num_colors(nc), color_count(col_count) {}
 
 std::string GameMaster::pretty_print_solution() {
   std::string solution_string;
-  for (int i=0; i<num_spaces; i++) {
+  for (int i = 0; i < num_spaces; i++) {
     // We add 97 so that each color is represented by an ASCII letter
     solution_string += solution[i] + 97;
   }
   return solution_string;
 }
 
+response GameMaster::evaluate_guess(const unsigned char guess[]) {
+  int perfect = 0;
+  int color_only = 0;
+  int color_count_guess[num_colors] = {0};
+  for (int i = 0; i < num_spaces; i++) {
+    if (guess[i] == solution[i]) {
+      perfect++;
+    }
+    color_count_guess[guess[i]]++;
+  }
+  for (int i = 0; i < num_colors; i++) {
+    color_only += std::min(color_count[i], color_count_guess[i]);
+  }
+  color_only -= perfect;
+  return response {perfect, color_only};
+}
--- gamemaster.h
+++ gamemaster.h
@@ -1,13 +1,19 @@
 #include <string>
 
-class GameMaster {
- public:
-  static GameMaster* random_solution(int, int);
-  std::string pretty_print_solution();
- private:
-  GameMaster(char*,int,int);
-  const char* solution;
-  const int num_spaces;
-  const int num_colors;
+struct response {
+  int perfect;
+  int color_only;
 };
 
+class GameMaster {
+ public:
+  static GameMaster* random_solution(int, unsigned char);
+  std::string pretty_print_solution();
+  response evaluate_guess(const unsigned char[]);
+ private:
+  GameMaster(unsigned char*,int,unsigned char,int*);
+  const unsigned char* solution;
+  const int num_spaces;
+  const unsigned char num_colors;
+  const int* color_count;
+};
