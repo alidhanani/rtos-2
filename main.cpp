173910dd2fd7ddae8ecb3a37e61688df1206a87b
chrislample <clample@crimson.ua.edu>
2019-12-11 22:54:57 +0000

    getting closer

--- main.cpp
+++ main.cpp
@@ -8,9 +8,9 @@
 #include "respondedguess.h"
 namespace mpi = boost::mpi;
 
-void run_gamemaster();
-void run_guesser(unsigned int, unsigned int);
-void report_response(const RespondedGuess&);
+void run_gamemaster(mpi::communicator world);
+void run_guesser(mpi::communicator world, unsigned int, unsigned int);
+void report_response(mpi::communicator world, const RespondedGuess&);
 
 int main(int argc, char** argv) {
   try {
@@ -18,10 +18,10 @@ int main(int argc, char** argv) {
     mpi::communicator world;
     if (world.rank() == 0) {
       std::cout << "Starting master " << world.rank() << std::endl;
-      run_gamemaster();
+      run_gamemaster(world);
     } else {
       std::cout << "Starting guesser " << world.rank() - 1 << std::endl;
-      run_guesser(world.rank() - 1, world.size() - 1);
+      run_guesser(world, world.rank() - 1, world.size() - 1);
     }
 
     std::cout << "Thread " << world.rank() << " signing off!" << std::endl;
@@ -34,40 +34,39 @@ int main(int argc, char** argv) {
   return 0;
 }
 
-void run_gamemaster() {
+void run_gamemaster(mpi::communicator world) {
   GameMaster master = GameMaster::with_random_solution(util::number_spaces, util::number_colors);
   std::cout << "Master using solution: " << master.solution.pretty_print() << std::endl;
     
-  util::response response;
+  RespondedGuess response;
   int guess_number = 0;
   do {
-    messages::proposed_guess proposed_guess;
-    MPI_Recv(&proposed_guess, 1, mpi_proposed_guess, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    ProposedGuess proposed_guess;
+    world.recv(mpi::any_source, 0, proposed_guess);
+    std::cout << "Received proposed guess: " << proposed_guess.guess_number << std::endl;
     if (proposed_guess.guess_number != guess_number) {
       // Ignore the proposed_guess since it is outdated
       std::cout << "Ignoring outdated guess" << std::endl;
       continue;
     }
 
-    std::vector<unsigned char> seq {std::begin(proposed_guess.guess), std::end(proposed_guess.guess)};
-    ColorSequence color_seq = {util::number_colors,seq};
-    response = master.evaluate_guess(color_seq);
-    report_response(color_seq, response, mpi_guess_response);
+    response = master.evaluate_guess(proposed_guess.color_sequence);
+    report_response(world, response);
     guess_number++;
   } while (response.perfect != util::number_spaces);
 }
 
-void report_response(const RespondedGuess& response) {
+void report_response(mpi::communicator world, const RespondedGuess& response) {
   std::cout
     << response.color_sequence.pretty_print()
     << " perfect: " << response.perfect
     << " color_only: " << response.color_only
     << std::endl;
 
-  MPI_Bcast(&res, 1, mpi_guess_response, 0, MPI_COMM_WORLD);
+  broadcast(world, response, 0);
 }
 
-void run_guesser(unsigned int id, unsigned int number_guessers) {
+void run_guesser(mpi::communicator world, unsigned int id, unsigned int number_guessers) {
   Guesser guesser = {id, number_guessers, util::number_colors, util::number_spaces};
   int guess_number = 0;
   
